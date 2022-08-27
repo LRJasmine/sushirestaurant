6d949545086448c3ced7ad792624de9fb9b6846c
Replit user <>
2022-08-27 00:02:04 +0000

    cartpage layout

--- app.js
+++ app.js
@@ -32,8 +32,35 @@ let menuitemsobj = [
     itemname: 'Burrito'
   }
 ]
+let cartitemsobj = [
+  {
+    cartitemimg: "rectangle_9",
+    cartitemname: "Smashed Avo",
+    cartitemprice: "20.00",
+    cartitemquantity: 1
+  },
+  {
+    cartitemimg: "rectangle_10",
+    cartitemname: "Yin & Yang",
+    cartitemprice: "10.00",
+    cartitemquantity: 1
+  },
+  {
+    cartitemimg: "rectangle_11",
+    cartitemname: "Pancakes",
+    cartitemprice: "25.00",
+    cartitemquantity: 1
+  },
+  {
+    cartitemimg: "rectangle_13",
+    cartitemname: "Rancheros (Tofu)",
+    cartitemprice: "25.00",
+    cartitemquantity: 1
+  }
+]
 
 const menuitemsdiv = document.getElementById('menulist');
+const cartitemsdiv = document.getElementById('cartlist');
 
 function loadMenu() {
   for (item of menuitemsobj) {
@@ -41,7 +68,7 @@ function loadMenu() {
     let img = document.createElement("img");
     let p = document.createElement("p");
     img.classList.add("menuimg");
-    p.classList.add("menuitemname");
+    -p.classList.add("menuitemname");
     let text = document.createTextNode(item.itemname);
     p.appendChild(text);
     img.src = "images/" + item.menuimgurl + ".png";
@@ -50,7 +77,69 @@ function loadMenu() {
     menuitemsdiv.appendChild(li);
   }
 }
-loadMenu();
+loadCart();
+function loadCart() {
+  for (item of cartitemsobj) {
+    let li = document.createElement("li");
+
+    let imgdiv = document.createElement("div");
+    imgdiv.classList.add("cartimgdiv");
+    let img = document.createElement("img");
+    img.src = "images/" + item.cartitemimg + ".png";
+    imgdiv.appendChild(img);
+    li.appendChild(imgdiv);
+
+    let namepricediv = document.createElement("div");
+    namepricediv.classList.add("namepricediv");
+
+    let itemname = document.createElement("p");
+    let itemnametext = document.createTextNode(item.cartitemname);
+    itemname.appendChild(itemnametext);
+    namepricediv.appendChild(itemname);
+
+    let itemprice = document.createElement("p");
+    let itempricetext = document.createTextNode("$" + item.cartitemprice);
+    itemprice.appendChild(itempricetext);
+    namepricediv.appendChild(itemprice);
+
+    li.appendChild(namepricediv);
+
+    let editcartitem = document.createElement("div");
+    editcartitem.classList.add("editcartitem");
+    let quantitydiv = document.createElement("div");
+    quantitydiv.classList.add("quantitydiv");
+    
+    let descquantitybtn = document.createElement("button");
+    let descbtntext = document.createTextNode("-");
+    descquantitybtn.appendChild(descbtntext);
+    quantitydiv.appendChild(descquantitybtn);
+    
+    let quantityp = document.createElement("p");
+    let quantitytext = document.createTextNode(item.cartitemquantity);
+    quantityp.appendChild(quantitytext);
+    quantitydiv.appendChild(quantityp);
+    
+    let incquantitybtn = document.createElement("button");
+    let incbtntext = document.createTextNode("+");
+    incquantitybtn.appendChild(incbtntext);
+    quantitydiv.appendChild(incquantitybtn);
+
+    editcartitem.appendChild(quantitydiv);
+
+    let removeitemdiv = document.createElement("div");
+    removeitemdiv.classList.add("removeitemdiv");
+    let removeitembtn = document.createElement("button");
+    let removeitemtext = document.createTextNode("X");
+    removeitembtn.appendChild(removeitemtext);
+    removeitemdiv.appendChild(removeitembtn);
+    editcartitem.appendChild(removeitemdiv);
+    
+    li.appendChild(editcartitem);
+    
+    cartitemsdiv.appendChild(li);
+  }
+}
+
 function goToMenu() {
   window.location.assign('/menu.html');
   loadMenu();
